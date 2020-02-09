749e6d34b417620f0ce708e7c6046fe94ba11d99
bollinca <bollinca522@gmail.com>
2020-02-09 14:44:15 -0500

    Create console log of api JSON data

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 fetch('https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=c905db381813af430e27cba76dfd46f7', {mode: 'cors'})
   .then(function(response){
     console.log(response);
+    return response.json();
+  })
+  .then(function(jsonResponse) {
+    console.log(jsonResponse);
   })
   .catch(function(err) {
     console.log(err);
