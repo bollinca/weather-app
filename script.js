3166d8644e2b3f4e3d7af6a7d37df06fcc8ba042
bollinca <bollinca522@gmail.com>
2020-02-09 15:13:24 -0500

    Add basic html structure, access JSON data through console

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
-fetch('https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=c905db381813af430e27cba76dfd46f7', {mode: 'cors'})
-  .then(function(response){
-    console.log(response);
-    return response.json();
-  })
-  .then(function(jsonResponse) {
-    console.log(jsonResponse);
-  })
-  .catch(function(err) {
-    console.log(err);
-  })
+async function fetchWeatherData() {
+  const response = await fetch('https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=c905db381813af430e27cba76dfd46f7', { mode: 'cors' })
+  const jsonData = await response.json();
+  console.log(jsonData.main);
+  console.log(jsonData.name);
+  console.log(jsonData.weather[0].main);
+}
+
+const displays = {
+  location: document.querySelector('#location'),
+  temperature: document.querySelector('#temperature'),
+}
+
+fetchWeatherData().catch(err => console.log(err));
