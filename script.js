3d308a7f49301147be2eac9c6d11d7db218db8ec
bollinca <bollinca522@gmail.com>
2020-02-10 14:26:39 -0500

    Display location and Kelvin temperature

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 async function fetchWeatherData() {
   const response = await fetch('https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=c905db381813af430e27cba76dfd46f7', { mode: 'cors' })
   const jsonData = await response.json();
-  console.log(jsonData.main);
-  console.log(jsonData.name);
-  console.log(jsonData.weather[0].main);
+  return jsonData;
+}
+
+async function updateDisplays() {
+  let jsonData = await fetchWeatherData().catch(err => console.log(err))
+  console.log(jsonData);
+  displays.location.textContent = `City: ${jsonData.name}`;
+  displays.temperature.textContent = `Current Temp: ${jsonData.main.temp}\u00B0K`
 }
 
 const displays = {
@@ -11,4 +16,4 @@ const displays = {
   temperature: document.querySelector('#temperature'),
 }
 
-fetchWeatherData().catch(err => console.log(err));
+updateDisplays();
