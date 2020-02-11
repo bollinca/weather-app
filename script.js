b3db5c7fd4f8521af12e265db15684ed9e5169fd
bollinca <bollinca522@gmail.com>
2020-02-11 15:31:32 -0500

    Make search functional for single word cities and countries

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-async function fetchWeatherData() {
-  const response = await fetch('https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=c905db381813af430e27cba76dfd46f7', { mode: 'cors' })
+async function fetchWeatherData(location) {
+  const response = await fetch('https://api.openweathermap.org/data/2.5/weather?q=' + `${location}` + '&APPID=c905db381813af430e27cba76dfd46f7', { mode: 'cors' })
   const jsonData = await response.json();
   return jsonData;
 }
 
-async function updateDisplays() {
-  let jsonData = await fetchWeatherData().catch(err => console.log(err))
+async function updateDisplays(location) {
+  let jsonData = await fetchWeatherData(location).catch(err => console.log(err))
   console.log(jsonData);
 
   let fahrenTemp = Math.round((jsonData.main.temp - 273.15) * (9 / 5) + 32);
@@ -13,19 +13,21 @@ async function updateDisplays() {
   displays.location.textContent = `City: ${jsonData.name}`;
 }
 
-const buttonControl = {
+const searchControl = {
+  field: document.querySelector('#search'),
+  submitButton: document.querySelector('#search-submit'),
 
-  formatInput: function () {
-    let currentValue = search.field.value;
-    let noCommaValue = currentValue.trim().replace(',', '');
-    let formattedValue = noCommaValue.replace(/\s/g, ',');
-    return formattedValue
+  formatInput: function () { // BUG - cannot accept cities/countries with spaces in name
+    let location = this.field.value;
+    let noCommaLocation = location.trim().replace(',', '');
+    let formattedLocation = noCommaLocation.replace(/\s/g, ',');
+    return formattedLocation
   },
 
   bindButton: function () {
-    search.submitButton.addEventListener('click', () => {
-      let value = this.formatInput();
-      console.log(value);
+    this.submitButton.addEventListener('click', () => {
+      let location = this.formatInput();
+      updateDisplays(location)
     })
   }
 }
@@ -35,10 +37,5 @@ const displays = {
   temperature: document.querySelector('#temperature'),
 }
 
-const search = {
-  field: document.querySelector('#search'),
-  submitButton: document.querySelector('#search-submit'),
-}
-
-buttonControl.bindButton();
-updateDisplays();
+searchControl.bindButton();
+updateDisplays('london, uk');
